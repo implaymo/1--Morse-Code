8b3f47667894e9b94a67b71062207c8adda4bacc
implaymo <136201112+implaymo@users.noreply.github.com>
2024-01-29 12:21:35 +0000

    Create Label to show the result of the Morse Code

--- ui.py
+++ ui.py
@@ -7,16 +7,26 @@ class UI():
         
         self.window = tk.Tk()
         self.window.title("Morse Code Converter")
-        self.window.geometry("200x200")
+        self.window.geometry("300x300")
         
         self.text = tk.Label(self.window, text="Convert text to Morse Code!")
-        self.text.grid(column=0, row=0)
+        self.text.grid(column=1, row=0)
+        
+        self.input = tk.Label(self.window, text="Input your text: ")
+        self.input.grid(column=0, row=1)
+        
+        self.result = tk.Label(self.window, text="Morse Code: ")
+        self.result.grid(column=0, row=2)
+        
+        self.morse_code_result = tk.Label(self.window)
+        self.morse_code_result.grid(column=1, row=2)
         
         self.string_entry = tk.Entry(self.window)
-        self.string_entry.grid(column=0, row=1)
+        self.string_entry.grid(column=1, row=1)
+        
         
         self.converter_button = tk.Button(self.window, text="Convert", command=self.string_to_morse_code)
-        self.converter_button.grid(column=0, row=2)
+        self.converter_button.grid(column=1, row=3)
         
         
         self.window.mainloop()
@@ -25,4 +35,7 @@ class UI():
     def string_to_morse_code(self):
         user_input = self.string_entry.get()
         final_code = [self.morse_code_dict[letter] for letter in user_input.upper()]
-        print(*final_code)
+        morse_code = ''.join(final_code)
+        self.morse_code_result.config(text=f"{morse_code}")
+        
+        
